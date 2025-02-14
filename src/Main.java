4f50064da84160dc461b058028e67c8cd15ffce8
JosueRS22 <146473545+JosueRS22@users.noreply.github.com>
2025-02-13 21:18:43 -0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,17 @@ public class Main {
         Entrenador entrenador1 = new Entrenador(002,"Javier","Aguirre",66, 20);
 
         Masajista masajista1 = new Masajista(003,"Luis", "Rosas", 40, "Fisioterapeuta", 7);
+
+        System.out.println(futbolista1.toString());
+        System.out.print(futbolista1.getNombre());
+        futbolista1.jugarPartido();
+
+        System.out.println(entrenador1.toString());
+        System.out.print(entrenador1.getApellido());
+        entrenador1.dirigirPartido();
+
+        System.out.println(masajista1.toString());
+        System.out.print("El " + masajista1.getProfesion());
+        masajista1.darMasaje();
     }
 }
